ed16058cc521fea0666988c2c3796aeebc0cff1f
sgtbrunner <guilherme.brunner@gmail.com>
2022-04-01 09:15:47 -0300

    adds app background styles

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
-import { StyleSheet } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 import StartGameScreen from './src/screens/StartGameScreen';
 
+const BackgroundImage = require('./assets/images/background.png');
+
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.root}>
+      <ImageBackground
+        source={BackgroundImage}
+        resizeMode='cover'
+        imageStyle={styles.backgroundImage}
+        style={styles.root}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  backgroundImage: {
+    opacity: 0.15,
   },
 });
